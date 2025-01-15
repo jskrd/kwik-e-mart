e4f0ae7a99f2525c2257ea7f62913d53d4f999ae
Joe S. <joe@skrd.io>
2025-01-15 16:49:37 +0000

    feat: add NAT Gateway config

--- deployments/terraform/modules/vpc/main.tf
+++ deployments/terraform/modules/vpc/main.tf
@@ -11,17 +11,9 @@ resource "aws_vpc" "main" {
   }
 }
 
-# Create Internet Gateway for public subnet internet access
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "${var.project_name}-${var.environment}"
-  }
-}
-
 resource "aws_subnet" "public" {
-  count             = length(var.availability_zones)
+  count = length(var.availability_zones)
+
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.${local.octet}.${count.index}.0/24"
   availability_zone = var.availability_zones[count.index]
@@ -32,7 +24,8 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  count             = length(var.availability_zones)
+  count = length(var.availability_zones)
+
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.${local.octet}.${count.index + 10}.0/24"
   availability_zone = var.availability_zones[count.index]
@@ -40,4 +33,79 @@ resource "aws_subnet" "private" {
   tags = {
     Name = "${var.project_name}-${var.environment}-private-${count.index + 1}"
   }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}"
+  }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-nat"
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}"
+  }
+
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-public"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-private"
+  }
+}
+
+resource "aws_default_route_table" "main" {
+  default_route_table_id = aws_vpc.main.default_route_table_id
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(aws_subnet.private)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
